7ae5a27005e8522108f74830a326283d5548930e
Rodrigo Augusto Bunhak Zwierzikowski <rodrigo.bunhak@gmail.com>
2020-06-11 10:26:10 -0300

    ajustado a lógica de conversão

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,22 @@
 const binaryInput = document.querySelector("input[name=binary]")
+const decimalInput = document.querySelector("input[name=decimal]")
 const binaryField = document.querySelector("#field-binary")
 const binarySpan = document.querySelector("span")
 
 
 binaryInput.addEventListener("keyup", function(event){
-  const keyPres = event.key
   const binaryNumber = event.target.value
-  
-  if(keyPres != 0 && keyPres != 1){
-      // binarySpan.textContent = ""
+
+  binaryNumber.split('').map((char) => {
+    if (char !== '0' && char !== '1') {
+      // Escreve um span informando que o valor digitado é invalido
+      binarySpan.textContent = ""
       const spanBinaryContent = document.createTextNode("Valor digitado é inválido!")
-      
       binarySpan.appendChild(spanBinaryContent)
-
-      // função limpa o span
-      setTimeout(function(){
-        binarySpan.textContent = ""
-      }, 2000)
-
-      binaryInput.value = ""
-
-  }
-  // console.log(binaryNumber)
+      decimalInput.value = ""
+    } else {
+      binarySpan.textContent = ""
+      decimalInput.value = parseInt(binaryInput.value, 2);
+    }
+  });
 })
