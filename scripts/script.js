d163fc320829d5103f11ea1166e654019f2e4c66
Rodrigo Augusto Bunhak Zwierzikowski <rodrigo.bunhak@gmail.com>
2020-06-11 01:25:35 -0300

    finalizado a lógica da entrada do valor

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,25 @@
 const binaryInput = document.querySelector("input[name=binary]")
 const binaryField = document.querySelector("#field-binary")
+const binarySpan = document.querySelector("span")
+
 
 binaryInput.addEventListener("keyup", function(event){
   const keyPres = event.key
   const binaryNumber = event.target.value
-
+  
   if(keyPres != 0 && keyPres != 1){
-    const spanBinary = document.createElement("span")
-    binaryField.appendChild(spanBinary)
+      // binarySpan.textContent = ""
+      const spanBinaryContent = document.createTextNode("Valor digitado é inválido!")
+      
+      binarySpan.appendChild(spanBinaryContent)
+
+      // função limpa o span
+      setTimeout(function(){
+        binarySpan.textContent = ""
+      }, 2000)
+
+      binaryInput.value = ""
+
   }
-  
-  console.log(binaryNumber)
-  
+  // console.log(binaryNumber)
 })
